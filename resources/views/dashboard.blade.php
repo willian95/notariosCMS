24951b9e69f008b352bd58f6fdb11c4accd4df47
patriciax <yively.pa@gmail.com>
2022-03-15 14:51:43 -0500

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,90 @@
 @extends("layouts.main")
+@section("content")
+<div class="d-flex flex-column-fluid" id="dev-format">
+    <div class="container">
+
+        <div class="row" style="margin-top: 10px; margin-bottom: 10px;">
+            <div class="col-md-6">
+                <div class="card card-custom bg-info bg-hover-state-info card-stretch card-stretch gutter-b bg-card" style="background-image: url(https://images.unsplash.com/photo-1560785219-cc81ab373cd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80);   ">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                            <!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
+                            <svg class="menu-icon " width="512" height="512" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                                <g data-name="Layer 2">
+                                    <path d="M27.62,27H4.38a3.28,3.28,0,0,1-1.54-.38,3.31,3.31,0,0,1-1.84-3V18.5a1,1,0,0,1,1-1H2a1,1,0,0,1,1,1v5.12a1.34,1.34,0,0,0,.75,1.22,1.32,1.32,0,0,0,.63.16H27.62A1.36,1.36,0,0,0,29,23.62V8.38A1.38,1.38,0,0,0,27.62,7H4.37A1.37,1.37,0,0,0,3,8.37v4.21a1,1,0,0,1-1,1H2a1,1,0,0,1-1-1V8.38A3.39,3.39,0,0,1,4.38,5H27.62A3.38,3.38,0,0,1,31,8.38V23.62A3.36,3.36,0,0,1,27.62,27Z" />
+                                    <path d="M22.19,27H4.38a1.45,1.45,0,0,1-1.18-.59l-1.08-.57.81-.93,6.62-8a2.61,2.61,0,0,1,2-1h0a2.61,2.61,0,0,1,2,.92ZM5.45,25H17.87L12,18.12a.62.62,0,0,0-.47-.22.61.61,0,0,0-.47.22Z" />
+                                    <path d="M27.62,27H19.57l-3.72-4.39,4.92-5.8a2.59,2.59,0,0,1,2-.93h0a2.59,2.59,0,0,1,2,.94l5.64,6.66a2.16,2.16,0,0,1,.37.6l.16.37-.15.38A3.37,3.37,0,0,1,27.62,27Zm-7.13-2h7.13a1.38,1.38,0,0,0,1-.47l-5.44-6.42a.6.6,0,0,0-.47-.22.59.59,0,0,0-.46.22l-3.83,4.51Z" />
+                                    <circle cx="17.16" cy="11.58" r="2" />
+                                </g>
+                            </svg>
+                            <!--end::Svg Icon-->
+                        </span>
+                        <div class="text-inverse-info font-weight-bolder font-size-h5 mb-2 mt-5">Proyectos: 1</div>
+                    </div>
+                    <!--end::Body-->
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div href="#" class="card card-custom bg-info bg-hover-state-info card-stretch card-stretch gutter-b  bg-card" style="background-image: url(https://images.unsplash.com/photo-1533488765986-dfa2a9939acd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80); background-position: center;">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <span class="svg-icon svg-icon-info svg-icon-3x ml-n1">
+                            <!--begin::Svg Icon | path:assets/media/svg/icons/Shopping/Cart3.svg-->
+                            <img width="40px" class="menu-icon" src="http://imgfz.com/i/vUMY8pb.png" alt="" srcset="">
+                            <!--end::Svg Icon-->
+                        </span>
+                        <div class="text-inverse-white font-weight-bolder font-size-h5 mb-2 mt-5">Directores: 1</div>
+                    </div>
+                    <!--end::Body-->
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div href="#" class="card card-custom bg-info bg-hover-state-info card-stretch card-stretch gutter-b  bg-card" style="background-image: url(https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=884&q=80);    background-position: 14% -292px;">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <span class="svg-icon svg-icon-info svg-icon-3x ml-n1">
+                            <!--begin::Svg Icon | path:assets/media/svg/icons/Shopping/Cart3.svg-->
+                            <svg class="menu-icon " xmlns="http://www.w3.org/2000/svg" width="512" height="24" fill="none" viewBox="0 0 24 24"><path fill="#000;" d="M18.0524 14.7625C18.138 14.8958 18.2619 14.9999 18.4047 15.0684C19.4963 15.5925 20.2499 16.7082 20.2499 18V20C20.2499 20.4142 20.5857 20.75 20.9999 20.75C21.4142 20.75 21.7499 20.4142 21.7499 20V18C21.7499 15.3766 19.6233 13.25 16.9999 13.25C16.8913 13.25 16.8423 13.3902 16.9245 13.4614C17.3597 13.838 17.7402 14.2763 18.0524 14.7625Z"/><path fill="#000;" fill-rule="evenodd" d="M7 14.75C5.20507 14.75 3.75 16.2051 3.75 18V20C3.75 20.4142 3.41421 20.75 3 20.75C2.58579 20.75 2.25 20.4142 2.25 20V18C2.25 15.3766 4.37665 13.25 7 13.25H13C15.6234 13.25 17.75 15.3766 17.75 18V20C17.75 20.4142 17.4142 20.75 17 20.75C16.5858 20.75 16.25 20.4142 16.25 20V18C16.25 16.2051 14.7949 14.75 13 14.75H7Z" clip-rule="evenodd"/><path fill="#000;" d="M14.8704 9.55748C14.9594 9.3884 15.1008 9.25336 15.2655 9.15651C16.0042 8.72208 16.5 7.91894 16.5 7C16.5 6.08106 16.0042 5.27792 15.2655 4.84349C15.1008 4.74664 14.9594 4.6116 14.8704 4.44252C14.6219 3.9701 14.3066 3.53827 13.9371 3.15954C13.8794 3.10037 13.9173 3 14 3C16.2091 3 18 4.79086 18 7C18 9.20914 16.2091 11 14 11C13.9173 11 13.8794 10.8996 13.9371 10.8405C14.3066 10.4617 14.6219 10.0299 14.8704 9.55748Z"/><path fill="#000;" fill-rule="evenodd" d="M10 9.5C11.3807 9.5 12.5 8.38071 12.5 7C12.5 5.61929 11.3807 4.5 10 4.5C8.61929 4.5 7.5 5.61929 7.5 7C7.5 8.38071 8.61929 9.5 10 9.5ZM10 11C12.2091 11 14 9.20914 14 7C14 4.79086 12.2091 3 10 3C7.79086 3 6 4.79086 6 7C6 9.20914 7.79086 11 10 11Z" clip-rule="evenodd"/></svg>
+                            <!--end::Svg Icon-->
+                        </span>
+                        <div class="text-inverse-white font-weight-bolder font-size-h5 mb-2 mt-5">Usuarios: 1</div>
+                    </div>
+                    <!--end::Body-->
+                </div>
+            </div>
+
+        </div>
+    </div>
+    @endsection
+
+    <style>
+        .bg-card {
+            position: relative;
+            background-position: top;
+            border-radius: 8px;
+
+        }
+
+        .bg-card::before {
+            content: "";
+            background: #00000059;
+            width: 100%;
+            position: absolute;
+            height: 100%;
+            border-radius: 4px;
+            z-index: 0;
+        }
+
+        .bg-card .font-size-h5 {
+            font-size: 1.25rem !important;
+            z-index: 5;
+            position: relative;
+            color: #fff !important;
+        }
+
+        .bg-card svg,
+        .bg-card img {
+            filter: invert(1);
+        }
+    </style>
