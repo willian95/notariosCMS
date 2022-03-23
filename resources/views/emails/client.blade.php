e2d3bbaf54192543d220cffd522c5dab12e3ea4a
willian rodriguez <rodriguezwillian95@gmail.com>
2022-03-23 08:10:42 -0400

    cambios

--- resources/views/emails/client.blade.php
+++ resources/views/emails/client.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html style="width:100%;   font-family: 'Poppins', sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0;" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
+<html style="width:100%;   font-family: 'Poppins', sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0;" xmlns="http://www.w3.org/1999/xhtml" lang="en">
  <head> 
   <meta charset="UTF-8"> 
   <meta content="width=device-width, initial-scale=1" name="viewport"> 
